924a6ab27840877e29c2c54baadc8cafbfbdeed3
jjaehyun2 <81456273+jjaehyun2@users.noreply.github.com>
2025-11-09 23:52:59 +0900

    3rd task

--- 3rd task/ignition/modules/MyToken.ts
+++ 3rd task/ignition/modules/MyToken.ts
@@ -3,6 +3,6 @@ import {buildModule} from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("MyTokenDeploy", (m) => {
     const myToken = m.contract("MyToken", ["My Token", "MTK", 18]);
 
-    return {myToken}; //감싸서
+    return {myToken}; 
     
 })
