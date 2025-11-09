5b3256b01bfb75e315ad6f8405bc6db2c71e9cab
jjaehyun2 <81456273+jjaehyun2@users.noreply.github.com>
2025-11-10 00:04:21 +0900

    3rd task

--- 3rd task/contracts/MultiManagedAccess.sol
+++ 3rd task/contracts/MultiManagedAccess.sol
@@ -1,15 +1,15 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.28;
-
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
 
 abstract contract MultiManagedAccess {
-
     uint constant MANAGER_NUMBERS = 5;
     address public owner;
     address[MANAGER_NUMBERS] public managers;
     bool[MANAGER_NUMBERS] public confirmed;
 
+    //manager0 --> confirmed0
+    //manager1 --> confirmed1
+    // ...
 
     constructor(address _owner, address[MANAGER_NUMBERS] memory _managers) {
         owner = _owner;
@@ -29,6 +29,18 @@ abstract contract MultiManagedAccess {
         }
     }
 
+    modifier onlyManager() {
+        bool found = false;
+        for (uint i = 0; i < MANAGER_NUMBERS; i++) {
+            if (msg.sender == managers[i]) {
+                found = true;
+                break;
+            }
+        }
+        require(found, "You are not a manager");
+        _;
+    }
+
     modifier onlyAllConfirmed() {
         require(allConfirmed(), "Not all confirmed yet");
         _;
@@ -46,13 +58,12 @@ abstract contract MultiManagedAccess {
     function confirm() external {
         bool found = false;
         for (uint i = 0; i < MANAGER_NUMBERS; i++) {
-            if (msg.sender == managers[i]) { 
+            if (msg.sender == managers[i]) {
                 confirmed[i] = true;
                 found = true;
                 break;
             }
         }
-        require(found, "You are not a managers");
+        require(found, "You are not a manager");
     }
-
 }
